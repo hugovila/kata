45339c172c175eb9458ae740e04e4fc90d927930
Hugo Vila <git@hugovila.nom.es>
2014-06-20 18:55:14 +0200

    passed test 7: return Fizz for number multiple 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,11 +10,14 @@ RSpec.describe "Fizzbuzz" do
   	expect(fb.fizzbuzz(1)).to eq(1)
   	expect(fb.fizzbuzz(2)).to eq(2)
   	expect(fb.fizzbuzz(4)).to eq(4)
+  	expect(fb.fizzbuzz(7)).to eq(7)
+  	expect(fb.fizzbuzz(8)).to eq(8)
   end
 
-  it "return Fizz for number 3 and 6" do
+  it "return Fizz for number multiple 3" do
   	expect(fb.fizzbuzz(3)).to eq("Fizz")
   	expect(fb.fizzbuzz(6)).to eq("Fizz")
+  	expect(fb.fizzbuzz(9)).to eq("Fizz")
   end
 
   it "return Buzz for number 5" do
