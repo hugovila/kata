6582f37db85eadbcd9e9a57311f51ea6e910d1df
Hugo Vila <git@hugovila.nom.es>
2014-06-23 18:10:51 +0200

    test 5 fail: return Buzz for number 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,10 +9,17 @@ RSpec.describe "FizzBuzz" do
   it "return the same number" do
   	expect(fb.fizzbuzz(1)).to eq(1)
   	expect(fb.fizzbuzz(2)).to eq(2)
+  	expect(fb.fizzbuzz(4)).to eq(4)
   end
 
   it "return Fizz for number 3" do
   	expect(fb.fizzbuzz(3)).to eq("Fizz")
+
+  end
+
+  it "return Buzz for number 5" do
+  	expect(fb.fizzbuzz(5)).to eq("Buzz")
+
   end
 
 end
