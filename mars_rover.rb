7618398285fef8650ee06cf1e083597b7f304008
Hugo Vila <git@hugovila.nom.es>
2014-06-27 13:18:22 +0200

    mars rover move forward four times for 'ffff'

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,13 +1,18 @@
 
 class MarsRover
 
-  attr_reader :direction
-  attr_accessor :position
+  attr_reader :position, :direction
 
   def initialize(position, direction)
     @position = position
     @direction = direction
   end
 
+  def move(commands)
+  	commands.each_char do |command|
+  		@position[0] += 1
+  	end
+  	position
+  end
 
 end
