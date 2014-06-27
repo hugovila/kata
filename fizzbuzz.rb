b931a496b8449f0ca95984d5a880a3642be87ca6
Hugo Vila <git@hugovila.nom.es>
2014-06-27 17:07:44 +0200

    more refactoring

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,32 +1,35 @@
 
 class FizzBuzz
   
+  FIZZ_NUMBER = 3
+  BUZZ_NUMBER = 5
+
   def fizzbuzz(number)
     result = number
-    result = "Fizz" if fizz?(number) || include_fizz?(number)
-    result = "Buzz" if buzz?(number)  || include_buzz?(number)
+    result = "Fizz" if fizz?(number)
+    result = "Buzz" if buzz?(number)
     result = "FizzBuzz" if fizzbuzz?(number)
     result
   end
 
   def fizz?(number)
-  	number % 3 == 0
-  end
-
-  def include_fizz?(number)
-  	number.to_s.include? "3"
+  	divisible?(number, FIZZ_NUMBER) || include?(number, FIZZ_NUMBER)
   end
 
   def buzz?(number)
-  	number % 5 == 0
-  end
-
-  def include_buzz?(number)
-    number.to_s.include? "5"
+  	divisible?(number, BUZZ_NUMBER) || include?(number, BUZZ_NUMBER)
   end
 
   def fizzbuzz?(number)
-  	number % 15 == 0
+  	fizz?(number) && buzz?(number)
+  end
+
+  def divisible?(number, other_number)
+    number % other_number == 0
+  end
+
+  def include?(number, other_number)
+    number.to_s.include? other_number.to_s
   end
 
 end
