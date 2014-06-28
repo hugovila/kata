7947840217a2ec5f97185c24b5b7f556782b780d
Hugo Vila <git@hugovila.nom.es>
2014-06-28 20:02:17 +0200

    MarsRover return its self position

--- marsrover.rb
+++ marsrover.rb
@@ -2,4 +2,13 @@
 
 class MarsRover
 
+  def initialize(position, direction)
+    @position = position
+    @direction = direction
+  end
+
+  def position
+  	@position
+  end
+
 end
--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -4,7 +4,8 @@ require './marsrover'
 RSpec.describe MarsRover do
 	
   it "return its self position" do
-    mr = MarsRover.new([2,4], :N)
+    mr = MarsRover.new([2, 4], :N)
+    expect(mr.position()).to eq([2, 4])
   end
 	
 end
