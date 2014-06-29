26cd712229e975e3b29d1ec071e715d29a22c417
Hugo Vila <git@hugovila.nom.es>
2014-06-29 13:08:55 +0200

    MarsRover return its own position

--- marsrover.rb
+++ marsrover.rb
@@ -2,4 +2,13 @@
 
 class MarsRover
 
+  def initialize(initial_position, initial_direction)
+    @position = initial_position
+    @direction = initial_direction
+  end
+
+  def position
+    @position
+  end
+
 end
