49a8cc1413b6118d38fb672321de1b750e162a97
Hugo Vila <git@hugovila.nom.es>
2014-06-29 15:51:19 +0200

    should move backward for 'b' commands: fail

--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -13,10 +13,15 @@ RSpec.describe MarsRover do
     expect(mr.direction()).to eq(:E)
   end
 
-  it "should move forward for 'f' command" do
+  it "should move forward for 'f' commands" do
     mr = MarsRover.new([2, 4], :N)
     expect(mr.move("fff")).to eq([2, 7])
     expect(mr.move("ffff")).to eq([2, 11])
   end
 	
+  it "should move backward for 'b' commands" do
+  	mr = MarsRover.new([2, 4], :E)
+  	expect(mr.move("bb")).to eq([2, 2])
+  end
+
 end
