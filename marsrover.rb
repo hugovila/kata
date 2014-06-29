6dbba49e29701d4ede4766f2aad81ebe7eae65f8
Hugo Vila <git@hugovila.nom.es>
2014-06-29 13:16:07 +0200

    MarsRover return its own direction

--- marsrover.rb
+++ marsrover.rb
@@ -2,13 +2,13 @@
 
 class MarsRover
 
+  attr_reader :position, :direction
+
   def initialize(initial_position, initial_direction)
     @position = initial_position
     @direction = initial_direction
   end
 
-  def position
-    @position
-  end
+  
 
 end
