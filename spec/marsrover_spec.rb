092cfb5b217dde3ebf118873b7c7b0057a5452e8
Hugo Vila <git@hugovila.nom.es>
2014-06-30 12:47:37 +0200

    MarsRover should move forward to 'f' commands: fail

--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe MarsRover do
   it "should return its own direction" do
   	expect(MarsRover.new([2, 4], :E).direction).to eq(:E)
   end
+  it "should move forward to 'f' commands" do
+  	expect(MarsRover.new([2, 4], :S).move("ff")).to eq([2, 6])
+  end
 end
