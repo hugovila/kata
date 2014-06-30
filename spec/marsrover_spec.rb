ec5da644fc7837eb34d7343cbe1469c9279042df
Hugo Vila <git@hugovila.nom.es>
2014-06-30 12:42:07 +0200

    MarsRover should return its own direction: fail

--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe MarsRover do
   it "should return its own position" do
     expect(MarsRover.new([2, 4], :N).position()).to eq([2, 4])
   end
+  it "should return its own direction" do
+  	expect(MarsRover.new([2, 4], :E).direction).to eq(:E)
+  end
 end
