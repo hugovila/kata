7668207581eabe56a2192cdd7fdf4452f5604150
Hugo Vila <git@hugovila.nom.es>
2014-07-01 08:00:41 +0200

    MarsRover return its own position

--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -2,6 +2,6 @@ require './marsrover'
 
 RSpec.describe MarsRover do
   it "should return its own position" do
-    expect(MarsRover.new([2, 4], :N)).to eq([2, 4])
+    expect(MarsRover.new([2, 4], :N).position).to eq([2, 4])
   end
 end
