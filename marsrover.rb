9e80baebe2dbfd72f964cb7ac603170f0a69e499
Hugo Vila <git@hugovila.nom.es>
2014-07-02 13:36:26 +0200

    MarsRover small refactoring

--- marsrover.rb
+++ marsrover.rb
@@ -3,6 +3,12 @@
 class MarsRover
 
   attr_reader :position, :direction
+  
+  MOVE_ONE = 1
+  FORWARD = "f"
+  BACKWARD = "b"
+  LEFT = "l"
+  RIGHT = "r"
 
   def initialize (position, direction)
     @position = position
@@ -11,12 +17,14 @@ class MarsRover
 
   def move(commands)
     commands.each_char do |command| 
-      @position[1] += 1 if command == "f"
-      @position[1] -= 1 if command == "b"
-      @position[0] -= 1 if command == "l"
-      @position[0] += 1 if command == "r"
+      @position[1] += MOVE_ONE if command == FORWARD
+      @position[1] -= MOVE_ONE if command == BACKWARD
+      @position[0] -= MOVE_ONE if command == LEFT
+      @position[0] += MOVE_ONE if command == RIGHT
      end
     position
   end
+
+  
 
 end
