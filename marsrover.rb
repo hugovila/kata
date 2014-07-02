7abd2a2f6cecb717c23b21900a5fcfa8c267aa35
Hugo Vila <git@hugovila.nom.es>
2014-07-02 12:53:14 +0200

    MarsRover return its own position and direction

--- marsrover.rb
+++ marsrover.rb
@@ -1,4 +1,12 @@
 
 
 class MarsRover
+
+  attr_reader :position, :direction
+
+  def initialize (position, direction)
+    @position = position
+    @direction = direction
+  end
+
 end
--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -3,7 +3,10 @@ require './marsrover'
 
 RSpec.describe MarsRover do 
   it "should return its own position" do
-    expect(MarRover.new([4,8, :N])).to eq([4,8]) 
+    expect(MarsRover.new([4,8], :N).position).to eq([4,8]) 
   end
-	
+	it "should return its own direction" do
+    expect(MarsRover.new([4,8], :N).direction).to eq(:N) 
+  end
+
 end
