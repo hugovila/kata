87707754f9eedbd73ee635a299bf17c8ea444c76
Hugo Vila <git@hugovila.nom.es>
2014-07-02 13:54:20 +0200

    MarsRover more small refactoring

--- marsrover.rb
+++ marsrover.rb
@@ -10,21 +10,42 @@ class MarsRover
   LEFT = "l"
   RIGHT = "r"
 
+
   def initialize (position, direction)
     @position = position
     @direction = direction
   end
 
+
+
   def move(commands)
     commands.each_char do |command| 
-      @position[1] += MOVE_ONE if command == FORWARD
-      @position[1] -= MOVE_ONE if command == BACKWARD
-      @position[0] -= MOVE_ONE if command == LEFT
-      @position[0] += MOVE_ONE if command == RIGHT
+      @position[1] += MOVE_ONE if forward?(command)
+      @position[1] -= MOVE_ONE if backward?(command)
+      @position[0] -= MOVE_ONE if left?(command)
+      @position[0] += MOVE_ONE if right?(command)
      end
-    position
+    @position
   end
 
-  
+  def command? 
+
+  end
+
+  def forward?(command)
+    command == FORWARD
+  end
+
+  def backward?(command)
+    command == BACKWARD
+  end
+
+  def left?(command)
+    command == LEFT
+  end
+
+  def right?(command)
+    command == RIGHT
+  end
 
 end
