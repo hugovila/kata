914e5b4621179a28e98be015995d562080273163
Hugo Vila <git@hugovila.nom.es>
2014-07-03 19:27:56 +0200

    Mars Rover return its own position and direction

--- marsrover.rb
+++ marsrover.rb
@@ -1,12 +1,17 @@
 
 class MarsRover
 
-  def initialize(the_position)
+  def initialize(the_position, the_direction)
     @the_position = the_position
+    @the_direction = the_direction
   end
 
   def position
     [0, 0]
+  end
+
+  def direction
+    :N
   end
 
 end
--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe MarsRover do
 
   it "take position and direction" do
     the_position = [0, 0]
-    mr = MarsRover.new(the_position)
+    mr = MarsRover.new(the_position, :N)
     expect(mr.position()).to eq(the_position)
+    expect(mr.direction()).to eq(:N)
   end
   
 end
