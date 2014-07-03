c78ee22f06aba0149f6c84ed0bbcd1d2ff3ff85a
Hugo Vila <git@hugovila.nom.es>
2014-07-03 19:22:23 +0200

    Mars Rover return its position

---
+++ marsrover.rb
@@ -0,0 +1,12 @@
+
+class MarsRover
+
+  def initialize(the_position)
+    @the_position = the_position
+  end
+
+  def position
+    [0, 0]
+  end
+
+end
--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -1,7 +1,12 @@
 
+require './marsrover'
 
 RSpec.describe MarsRover do
 
-  
+  it "take position and direction" do
+    the_position = [0, 0]
+    mr = MarsRover.new(the_position)
+    expect(mr.position()).to eq(the_position)
+  end
   
 end
