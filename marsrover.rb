aadee99abb4a8a2f246ec103de557d3fc448f603
Hugo Vila <git@hugovila.nom.es>
2014-07-04 12:35:54 +0200

    configuring .rspec to --format documentation

--- marsrover.rb
+++ marsrover.rb
@@ -1,10 +1,14 @@
 
 class MarsRover
 
+  @POSITION_Y = 1
+  @SPEED      = 1
+
   def initialize(the_position = [0, 0], the_direction = :N)
     @the_position = the_position
     @the_direction = the_direction
   end
+
 
   def where_are_you
     @the_position
@@ -12,6 +16,12 @@ class MarsRover
 
   def what_are_you_viewing
     @the_direction
+  end
+
+  def move(commands)
+
+    @the_position[1] += 1
+    @the_position
   end
 
 end
