5e1a06248345a6b794598148438325c389f57107
Hugo Vila <git@hugovila.nom.es>
2014-07-10 00:13:45 +0200

    moved class Spinner to file spinner.rb

--- marsrover.rb
+++ marsrover.rb
@@ -1,6 +1,7 @@
 
 class MarsRover
 
+  require './spinner'
 
   POSITION_X = 0
   POSITION_Y = 1
@@ -67,38 +68,3 @@ class MarsRover
 
 end
 
-class Spinner
-
-  WEST = :W
-  EAST = :E
-  NORTH = :N
-  SOUTH = :S
-
-  def initialize(the_direction = :N)
-
-    @the_direction = the_direction
-
-    @array_direction = [NORTH, WEST, SOUTH, EAST]
-
-    @hash_directions = { NORTH => 0, WEST => 1, SOUTH => 2, EAST => 3 }
-
-    @the_machine_direction = @hash_directions[@the_direction]
-
-  end
-
-  def what_are_you_viewing
-    @the_direction
-  end
-
-  def spin_left
-    @the_machine_direction += 1
-    @the_direction = @array_direction[normalize_the_direction]
-  end
-
-  private
-
-  def normalize_the_direction
-    @the_machine_direction % 4
-  end
-
-end
